46695d35c488f7e5635e4d105635cff1c904fc0b
unisanta <unisanta@santacecilia.br>
2025-04-25 19:39:34 -0300

    Initial Commit

---
+++ ThreadAula/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadAula
+{
+    internal class Program
+    {
+        // Meu software é um processo e o main é minha Thread principal.
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Salve!");
+            Console.ReadKey();
+        }
+    }
+}
